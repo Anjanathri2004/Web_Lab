cae5708f4ee1ab0303d9587ea818d6db7603c898
Anjanathri2004 <161334855+Anjanathri2004@users.noreply.github.com>
2024-11-28 15:00:39 +0530

    Update URLRewritingConfirmationServlet.java

--- EX5/5.4/URLRewritingConfirmationServlet.java
+++ EX5/5.4/URLRewritingConfirmationServlet.java
@@ -5,20 +5,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/urlRewritingConfirmation")
+@WebServlet("/URLRewritingConfirmation")
 public class URLRewritingConfirmationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String name = request.getParameter("name");
-        String cuisine = request.getParameter("cuisine");
+        // Retrieve URL parameters
+        String customerName = request.getParameter("customerName");
+        String gadget = request.getParameter("gadget");
         String quantity = request.getParameter("quantity");
 
+        // Generate confirmation response
         response.setContentType("text/html");
         response.getWriter().println("<html><body>");
-        response.getWriter().println("<h1>Thank you, " + name + "</h1>");
-        response.getWriter().println("<p>Your order for " + quantity + " " + cuisine + " has been placed using URL Rewriting.</p>");
+        response.getWriter().println("<h1>Thank you, " + customerName + "</h1>");
+        response.getWriter().println("<p>Your order for " + quantity + " " + gadget + "(s) has been placed using URL Rewriting.</p>");
         response.getWriter().println("</body></html>");
     }
 }
